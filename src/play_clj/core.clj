8b715901258c8499f3b198ebf2123a13c0e94ad5
oakes <zsoakes@gmail.com>
2014-01-01 04:10:26 -0500

    Don't store width/height in screen map

--- src/play_clj/core.clj
+++ src/play_clj/core.clj
@@ -22,14 +22,11 @@
 (defn defscreen*
   [{:keys [on-show on-render on-hide on-pause on-resize on-resume]
     :or {on-show dummy on-render dummy on-hide dummy
-         on-pause dummy on-resize dummy on-resume dummy}
-    :as options}]
+         on-pause dummy on-resize dummy on-resume dummy}}]
   (let [screen (atom {})]
     (proxy [Screen] []
       (show []
         (on-show (swap! screen assoc
-                        :width (game :width)
-                        :height (game :height)
                         :total-time 0
                         :delta-time 0
                         :set-entities #(swap! screen assoc :entities %)
@@ -43,7 +40,7 @@
                           :delta-time delta-time)))
       (hide [] (on-hide @screen))
       (pause [] (on-pause @screen))
-      (resize [w h] (on-resize (swap! screen assoc :width w :height h)))
+      (resize [w h] (on-resize @screen))
       (resume [] (on-resume @screen)))))
 
 (defmacro defscreen
@@ -51,8 +48,7 @@
   `(defonce ~name (defscreen* ~options)))
 
 (defn defgame*
-  [{:keys [on-create]
-    :or {on-create dummy}}]
+  [{:keys [on-create] :or {on-create dummy}}]
   (proxy [Game] []
     (create [] (on-create this))))
 
