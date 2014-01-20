bcf2a706cf964981c26f70a1ff81952d2e063bf0
oakes <zsoakes@gmail.com>
2014-01-20 00:36:10 -0500

    Clean repo

--- template/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject play-clj/lein-template "0.1.0-SNAPSHOT"
-  :description "A template for making play-clj projects"
-  :url "https://github.com/oakes/play-clj"
-  :license {:name "Public Domain"
-            :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[lein-droid "0.2.0"]]
-  :resource-paths ["resources"]
-  :eval-in-leiningen true)
