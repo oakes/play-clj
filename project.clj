10f1a071c3be7e283bdc6b802788595f54b25eae
oakes <zsoakes@gmail.com>
2014-03-24 01:12:23 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject play-clj "0.2.3-SNAPSHOT"
+(defproject play-clj "0.2.3"
   :description "A LibGDX wrapper for easy cross-platform game development"
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[com.badlogicgames.gdx/gdx "0.9.9"]
                  [org.clojure/clojure "1.5.1"]]
-  :repositories [["sonatype"
-                  "https://oss.sonatype.org/content/repositories/snapshots/"]]
   :plugins [[lein-marginalia "0.7.1"]])
--- template/project.clj
+++ template/project.clj
@@ -1,4 +1,4 @@
-(defproject play-clj/lein-template "0.2.2"
+(defproject play-clj/lein-template "0.2.3"
   :description "A template for making play-clj projects"
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
