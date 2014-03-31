1cc40a3b3285919900266d46d0fc73c9eb294977
oakes <zsoakes@gmail.com>
2014-03-31 12:28:13 -0400

    Show namespaces

--- doclet/src/clojure/play_clj_doclet/html.clj
+++ doclet/src/clojure/play_clj_doclet/html.clj
@@ -16,19 +16,23 @@
     [:b (str name)]
     " "
     (string/join ", " (map param args))]
-   (when text [:i text])])
+   (when text
+     [:i text])])
 
 (defn create-from-file
-  [parsed-file]
-  (for [group (:groups parsed-file)]
-    [:div
-     [:h1 {} (:name group)]
-     (:docstring group)
-     (for [[name items] (:java group)]
-       [:div
-        (when (not= (:name group) name)
-          [:h3 name])
-        (map item items)])]))
+  [{:keys [ns groups] :as parsed-file}]
+  [:div
+   (when (and ns (> (count groups) 0))
+     [:h1 ns])
+   (for [group groups]
+     [:div
+      [:h2 {} (:name group)]
+      (:docstring group)
+      (for [[name items] (:java group)]
+        [:div
+         (when (not= (:name group) name)
+           [:h3 name])
+         (map item items)])])])
 
 (defn create
   [parsed-files]
