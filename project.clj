7b1e201cbd38df66d268e21da3d55619bcba0575
oakes <zsoakes@gmail.com>
2014-03-31 23:30:25 -0400

    Remove marg

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[com.badlogicgames.gdx/gdx "0.9.9"]
-                 [org.clojure/clojure "1.5.1"]]
-  :plugins [[lein-marginalia "0.7.1"]])
+                 [org.clojure/clojure "1.5.1"]])
