52d5322b20a9565e7927bd04ee8f0e5a77f946ae
oakes <zsoakes@gmail.com>
2014-04-11 14:16:41 -0400

    Update template for RoboVM 0.0.11

--- template/src/leiningen/new/play_clj/IOSLauncher.java
+++ template/src/leiningen/new/play_clj/IOSLauncher.java
@@ -3,17 +3,16 @@ package {{package}};
 import clojure.lang.RT;
 import clojure.lang.Symbol;
 
-import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
-import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.backends.iosrobovm.*;
 
-import org.robovm.cocoatouch.foundation.NSAutoreleasePool;
-import org.robovm.cocoatouch.uikit.UIApplication;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.uikit.*;
 
 public class {{ios-class-name}} extends IOSApplication.Delegate {
 	protected IOSApplication createApplication() {
 		IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-          RT.var("clojure.core", "require").invoke(Symbol.intern("{{namespace}}"));
+		RT.var("clojure.core", "require").invoke(Symbol.intern("{{namespace}}"));
 		try {
 			Game game = (Game) RT.var("{{namespace}}", "{{app-name}}").deref();
 			return new IOSApplication(game, config);
@@ -26,6 +25,6 @@ public class {{ios-class-name}} extends IOSApplication.Delegate {
 	public static void main(String[] argv) {
 		NSAutoreleasePool pool = new NSAutoreleasePool();
 		UIApplication.main(argv, null, {{ios-class-name}}.class);
-		pool.drain();
+		pool.close();
 	}
 }
