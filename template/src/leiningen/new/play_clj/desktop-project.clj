524839cf8bfe047ba778a1712f68efc12d59a445
oakes <zsoakes@gmail.com>
2014-04-18 17:39:54 -0400

    Update template to use bullet and box2d extensions

--- template/src/leiningen/new/play_clj/desktop-project.clj
+++ template/src/leiningen/new/play_clj/desktop-project.clj
@@ -3,6 +3,12 @@
   
   :dependencies [[com.badlogicgames.gdx/gdx "1.0"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "1.0"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.0"]
+                 [com.badlogicgames.gdx/gdx-box2d-platform "1.0"
+                  :classifier "natives-desktop"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.0"]
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.0"
+                  :classifier "natives-desktop"]
                  [com.badlogicgames.gdx/gdx-platform "1.0"
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.6.0"]
--- template/src/leiningen/new/play_clj/ios-project.clj
+++ template/src/leiningen/new/play_clj/ios-project.clj
@@ -2,13 +2,15 @@
   :description "FIXME: write description"
   :dependencies [[com.badlogicgames.gdx/gdx "1.0"]
                  [com.badlogicgames.gdx/gdx-backend-robovm "1.0"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.0"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.0"]
                  [org.clojure/clojure "1.6.0"]
                  [play-clj "0.3.0"]]
   :source-paths ["src/clojure" "../desktop/src-common"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
   :ios {:robovm-opts ["-forcelinkclasses" "{{package-prefix}}.**:clojure.**:com.badlogic.**:play_clj.**"
-                      "-libs" "libs/libObjectAL.a:libs/libgdx.a"
+                      "-libs" "libs/libObjectAL.a:libs/libgdx.a:libs/libgdx-box2d.a:libs/libgdx-bullet.a"
                       "-frameworks" "UIKit:OpenGLES:QuartzCore:CoreGraphics:OpenAL:AudioToolbox:AVFoundation"
                       "-resources" "../desktop/resources/**"]}
   :aot :all
