15d745a4bbdeb34f89c43a0bf8a929bbe52583c3
oakes <zsoakes@gmail.com>
2014-04-20 22:52:19 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject play-clj "0.3.0-SNAPSHOT"
+(defproject play-clj "0.3.0"
   :description "A LibGDX wrapper for easy cross-platform game development"
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
