0acc58a2c4666958cec63341838b17ddbf797494
oakes <zsoakes@gmail.com>
2014-05-16 18:26:37 -0400

    Increment version number

--- template/src/leiningen/new/play_clj/android-project.clj
+++ template/src/leiningen/new/play_clj/android-project.clj
@@ -7,7 +7,7 @@
                  [com.badlogicgames.gdx/gdx-bullet "1.0.1"]
                  [neko/neko "3.0.1"]
                  [org.clojure-android/clojure "1.6.0-RC1" :use-resources true]
-                 [play-clj "0.3.3"]]
+                 [play-clj "0.3.4"]]
   :profiles {:dev {:dependencies [[android/tools.nrepl "0.2.0-bigstack"]
                                   [compliment "0.0.4"]]
                    :android {:aot :all-with-unused}}
--- template/src/leiningen/new/play_clj/desktop-project.clj
+++ template/src/leiningen/new/play_clj/desktop-project.clj
@@ -12,7 +12,7 @@
                  [com.badlogicgames.gdx/gdx-platform "1.0.1"
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.3.3"]]
+                 [play-clj "0.3.4"]]
   
   :source-paths ["src" "src-common"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
