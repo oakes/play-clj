24eeb208ea9cf078d000f655f445703c1ce781e8
oakes <zsoakes@gmail.com>
2014-05-17 10:05:34 -0400

    Update compliment

--- template/src/leiningen/new/play_clj/android-project.clj
+++ template/src/leiningen/new/play_clj/android-project.clj
@@ -9,7 +9,7 @@
                  [org.clojure-android/clojure "1.6.0-RC1" :use-resources true]
                  [play-clj "0.3.4"]]
   :profiles {:dev {:dependencies [[android/tools.nrepl "0.2.0-bigstack"]
-                                  [compliment "0.0.4"]]
+                                  [compliment "0.1.0"]]
                    :android {:aot :all-with-unused}}
              :release {:android
                        {;; Specify the path to your private
