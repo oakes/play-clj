0dc673ea9be222c20818151f47fdb68b59f03134
oakes <zsoakes@gmail.com>
2014-05-23 11:48:11 -0400

    Increment version number

--- template/project.clj
+++ template/project.clj
@@ -1,4 +1,4 @@
-(defproject play-clj/lein-template "0.3.4"
+(defproject play-clj/lein-template "0.3.5"
   :description "A template for making play-clj projects"
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
--- template/src/leiningen/new/play_clj/desktop-project.clj
+++ template/src/leiningen/new/play_clj/desktop-project.clj
@@ -12,7 +12,7 @@
                  [com.badlogicgames.gdx/gdx-platform "1.0.1"
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.3.4"]]
+                 [play-clj "0.3.5"]]
   
   :source-paths ["src" "src-common"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
