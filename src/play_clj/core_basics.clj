84a314e0b065fcba2ec94ecd2e3ec58a5a754926
oakes <zsoakes@gmail.com>
2014-06-13 12:50:36 -0400

    Add gl macro

--- src/play_clj/core_basics.clj
+++ src/play_clj/core_basics.clj
@@ -60,6 +60,13 @@
   [k & options]
   `(u/call! ^GL20 (Gdx/gl20) ~k ~@options))
 
+(defmacro gl
+  "Returns a static field from [Gdx.gl20](http://libgdx.badlogicgames.com/nightlies/docs/api/com/badlogic/gdx/graphics/GL20.html).
+
+    (gl :gl-triangles)"
+  [k]
+  `~(u/gdx-field :graphics :GL20 (u/key->upper k)))
+
 (defmacro graphics!
   "Calls a single method on [Gdx.graphics](http://libgdx.badlogicgames.com/nightlies/docs/api/com/badlogic/gdx/Graphics.html).
 
