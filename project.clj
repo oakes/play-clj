04be0f1727e4df6ed992a6ede163cfc7d20e2d3e
oakes <zsoakes@gmail.com>
2014-06-22 14:46:12 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject play-clj "0.3.8-SNAPSHOT"
+(defproject play-clj "0.3.8"
   :description "A LibGDX wrapper for easy cross-platform game development"
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[com.badlogicgames.gdx/gdx "1.1.0"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.1.0"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.1.0"]
+  :dependencies [[com.badlogicgames.gdx/gdx "1.2.0"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.2.0"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.2.0"]
                  [org.clojure/clojure "1.6.0"]]
   :source-paths ["src"]
   :java-source-paths ["src-java"]
