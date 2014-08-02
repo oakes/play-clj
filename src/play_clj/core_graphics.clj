1ed5ff9a2afe1c4956f6c602a30426a249805c8d
oakes <zsoakes@gmail.com>
2014-08-02 17:28:03 -0400

    Use more efficient way to get layer

--- src/play_clj/core_graphics.clj
+++ src/play_clj/core_graphics.clj
@@ -116,11 +116,10 @@ Normally, you don't need to use this directly."
   ([screen layer]
     (if (isa? (type layer) MapLayer)
         layer
-        (->> ^BatchTiledMapRenderer (u/get-obj screen :renderer)
-             .getMap
-             .getLayers
-             (drop-while #(not= layer (.getName ^MapLayer %)))
-             first))))
+        (-> ^BatchTiledMapRenderer (u/get-obj screen :renderer)
+            .getMap
+            .getLayers
+            (.get layer)))))
 
 (defmacro tiled-map-layer
   "Returns a [TiledMapTileLayer](http://libgdx.badlogicgames.com/nightlies/docs/api/com/badlogic/gdx/maps/tiled/TiledMapTileLayer.html)
