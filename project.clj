16b4cbcf91f1d635ac37e84ac98091cea03a7f26
oakes <zsoakes@gmail.com>
2014-09-08 00:45:56 -0400

    Start 0.3.12-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject play-clj "0.3.11"
+(defproject play-clj "0.3.12-SNAPSHOT"
   :description "A libGDX wrapper for easy cross-platform game development"
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
