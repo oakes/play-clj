94a4c2d2df899aa2eac3da0d10d7beca79f9ed58
oakes <zsoakes@gmail.com>
2014-12-21 15:36:11 -0500

    Increment version number

--- template/project.clj
+++ template/project.clj
@@ -1,4 +1,4 @@
-(defproject play-clj/lein-template "0.4.3-SNAPSHOT"
+(defproject play-clj/lein-template "0.4.3"
   :description "A template for making play-clj projects"
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
