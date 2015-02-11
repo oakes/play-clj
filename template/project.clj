5c69689d379034efd76e09bda9a87da5353a04c9
oakes <zsoakes@gmail.com>
2015-02-11 14:55:20 -0500

    Update libGDX to 1.5.3

--- template/project.clj
+++ template/project.clj
@@ -1,8 +1,8 @@
-(defproject play-clj/lein-template "0.4.3"
+(defproject play-clj/lein-template "0.4.4"
   :description "A template for making play-clj projects"
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[lein-droid "0.3.0-beta4"]]
+  :dependencies [[lein-droid "0.3.1"]]
   :resource-paths ["resources"]
   :eval-in-leiningen true)
--- template/src/leiningen/new/play_clj/ios-project.clj
+++ template/src/leiningen/new/play_clj/ios-project.clj
@@ -1,11 +1,11 @@
 (defproject {{app-name}} "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[com.badlogicgames.gdx/gdx "1.5.0"]
-                 [com.badlogicgames.gdx/gdx-backend-robovm "1.5.0"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.5.0"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.5.0"]
+  :dependencies [[com.badlogicgames.gdx/gdx "1.5.3"]
+                 [com.badlogicgames.gdx/gdx-backend-robovm "1.5.3"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.5.3"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.5.3"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.4.3"]]
+                 [play-clj "0.4.4"]]
   :source-paths ["src/clojure" "../desktop/src-common"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
