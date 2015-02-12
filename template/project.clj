78f8062c92b86c11101757c63c492d4471c8c23a
oakes <zsoakes@gmail.com>
2015-02-11 23:08:52 -0500

    Update lein-droid

--- template/project.clj
+++ template/project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[lein-droid "0.3.1"]]
+  :dependencies [[lein-droid "0.3.3"]]
   :resource-paths ["resources"]
   :eval-in-leiningen true)
