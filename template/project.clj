e74f1ad5d50a9325a485833b8930a81801a4992b
oakes <zsoakes@gmail.com>
2015-03-18 12:31:45 -0400

    Update lein-droid in template

--- template/project.clj
+++ template/project.clj
@@ -1,8 +1,8 @@
-(defproject play-clj/lein-template "0.4.4"
+(defproject play-clj/lein-template "0.4.5-SNAPSHOT"
   :description "A template for making play-clj projects"
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[lein-droid "0.3.3"]]
+  :dependencies [[lein-droid "0.3.5"]]
   :resource-paths ["resources"]
   :eval-in-leiningen true)
