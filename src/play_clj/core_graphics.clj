bb7de511154dca3ba735ef1bfe28983196d62eed
oakes <zsoakes@gmail.com>
2016-01-02 10:39:49 -0500

    Don't draw actor entities twice

--- src/play_clj/core_graphics.clj
+++ src/play_clj/core_graphics.clj
@@ -492,13 +492,14 @@ with the tiled map file at `path` and `unit` scale.
   (let [^Batch batch (.getBatch renderer)]
     (.begin batch)
     (doseq [{:keys [additive? opacity] :as entity} entities]
-      (when additive?
-        (.setBlendFunction ^Batch batch (gl :gl-src-alpha) (gl :gl-one)))
-      (.setColor batch (color 1 1 1 (or opacity 1.0)))
-      (e/draw! entity screen batch)
-      (.setColor batch (color 1 1 1 1))
-      (when additive?
-        (.setBlendFunction ^Batch batch (gl :gl-src-alpha) (gl :gl-one-minus-src-alpha))))
+      (when-not (instance? Actor (u/get-obj entity :object))
+        (when additive?
+          (.setBlendFunction ^Batch batch (gl :gl-src-alpha) (gl :gl-one)))
+        (.setColor batch (color 1 1 1 (or opacity 1.0)))
+        (e/draw! entity screen batch)
+        (.setColor batch (color 1 1 1 1))
+        (when additive?
+          (.setBlendFunction ^Batch batch (gl :gl-src-alpha) (gl :gl-one-minus-src-alpha)))))
     (.end batch))
   entities)
 
